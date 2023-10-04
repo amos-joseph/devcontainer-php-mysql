607a60b4da4f6070b18d09eba86ca2c899a00768
Amos Joseph <amos.joseph@doane.edu>
2023-10-04 21:41:51 +0000

    Initial mariadb config working

--- .devcontainer/scripts/startup.sh
+++ .devcontainer/scripts/startup.sh
@@ -1,3 +1,4 @@
+/usr/share/scripts/install-mariadb.sh &
 php-fpm81
 chmod 777 /var/run/php/php8-fpm.sock 
 chmod 755 /usr/share/nginx/html/* 
--- website-data/index.php
+++ website-data/index.php
@@ -1,1 +1,14 @@
-<?php phpinfo(); ?>
+<?php 
+phpinfo(); 
+
+echo "Databases";
+$pdo = new PDO("mysql:host=localhost", "drupaladmin", "drupalpw");
+$sql = 'SHOW DATABASES';
+
+$q = $pdo->query($sql);
+$q->setFetchMode(PDO::FETCH_ASSOC);
+while( ( $db = $q->fetchColumn( 0 ) ) !== false ){
+    echo $db.'<br>';
+}
+
+?>
